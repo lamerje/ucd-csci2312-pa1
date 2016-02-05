eed1e67eb36ffd14b1e8de26d27819f48025d5ed
stargazer <jeffrey.lamer@ucdenver.edu>
2016-02-05 12:43:42 -0700

    early commit

--- area.cpp
+++ area.cpp
@@ -7,6 +7,22 @@
 
 
 {
+    double area;
+    Point p;
+    p p1, p2, p3;
 
+    p1.setX(X = 0.0);
+    p1.setY(Y = 0.0);
+    p1.setZ(Z = 0.1);
+
+    p2.setX(X = 15.0);
+    p2.setY(Y = 23.0);
+    p2.setZ(Z = 0.10);
+
+    p3.setX(X = 15.0);
+    p3.setY(Y = 0.0);
+    p3.setZ(Z = 0.1);
+
+area = sqrt(pow(p1, 2.0)+pow(p2, 2.0));
 
 }
